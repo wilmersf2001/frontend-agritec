d1e37c02e4db06054b4631e0c727b1cc4ab5265c
wilmersf2001 <wilmersf2001@users.noreply.github.com>
2024-03-05 00:25:01 -0500

    Agregadas importaciones y estilos en componentes de inicio y login-comprador

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss',
 })
--- src/app/pages/login-comprador/login-comprador.component.ts
+++ src/app/pages/login-comprador/login-comprador.component.ts
@@ -6,6 +6,12 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { Router } from '@angular/router';
+import {
+  FormGroup,
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-comprador',
@@ -17,12 +23,42 @@ import { Router } from '@angular/router';
     MatDividerModule,
     MatButtonModule,
     MatCheckboxModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login-comprador.component.html',
   styleUrl: './login-comprador.component.scss',
 })
 export class LoginCompradorComponent {
   hide = true;
+  form!: FormGroup;
+  loading = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      nombres: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: ['', Validators.required],
+      usuario: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return Object.values(this.form.controls).forEach((control) => {
+        control.markAsTouched();
+      });
+    }
+    console.log(this.form.value);
+  }
+
+  getFieldInvalid(fieldName: string) {
+    const control = this.form.get(fieldName);
+    return control?.touched && control?.invalid;
+  }
 }
